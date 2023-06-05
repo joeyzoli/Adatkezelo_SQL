d2e66d7a313c80fdbcb8f4a176f6361273edd9b8
joeyzoli <joeyayeti@gmail.com>
2023-06-05 15:09:47 +0200

    excel összefűz
    új excel összefűző függvény

--- src/Torlo.java
+++ src/Torlo.java
@@ -2,6 +2,7 @@ import javax.swing.JPanel;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -70,7 +71,7 @@ public class Torlo extends JPanel
 		
 		JButton feltolt = new JButton("feltölt");
 		feltolt.setBounds(412, 268, 77, 23);
-		feltolt.addActionListener(new IFS());
+		feltolt.addActionListener(new Excel_osszefuz());
 		setBackground(Foablak.hatter_szine);
 		setLayout(null);
 		add(lblNewLabel);
@@ -432,6 +433,150 @@ public class Torlo extends JPanel
                   con.close();  
                   Foablak.frame.setCursor(null);  
                   }           
+            catch(Exception e1)
+            { 
+                System.out.println(e1);
+                e1.printStackTrace();
+                String hibauzenet2 = e1.toString();
+                JOptionPane.showMessageDialog(null, hibauzenet2, "Hiba üzenet", 2);                                                 //kiírja a hibaüzenetet
+            }                                         
+         }
+    }
+	
+	class Excel_osszefuz implements ActionListener                                                                                      //csv-t gyárt a gomb
+    {
+        public void actionPerformed(ActionEvent e)
+         {
+            try
+            {
+                String excelfile1 = System.getProperty("user.home") + "\\Desktop\\Átforgatások 20220601-20230531.xlsx";              
+                String excelfile2 = System.getProperty("user.home") + "\\Desktop\\Beszerzési cikk szállítója.xlsx";
+                String excelfile3 = System.getProperty("user.home") + "\\Desktop\\Cikk katalógus.xlsx";
+                Foablak.frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));                
+                Workbook workbook = new Workbook();
+                workbook.loadFromFile(excelfile1);
+                Workbook workbook2 = new Workbook();
+                workbook2.loadFromFile(excelfile2);
+                Workbook workbook3 = new Workbook();
+                workbook3.loadFromFile(excelfile3);
+                Workbook workbook4 = new Workbook();
+                Worksheet sheet = workbook.getWorksheets().get(0);
+                Worksheet sheet2 = workbook2.getWorksheets().get(0);
+                Worksheet sheet3 = workbook3.getWorksheets().get(0);
+                Worksheet sheet4 = workbook4.getWorksheets().get(0);
+                DataTable datatable = new DataTable();
+                datatable = sheet.exportDataTable(sheet.getAllocatedRange(), false, false );
+                DataTable datatable2 = new DataTable();
+                datatable2 = sheet2.exportDataTable(sheet2.getAllocatedRange(), false, false );
+                DataTable datatable3 = new DataTable();
+                datatable3 = sheet3.exportDataTable(sheet3.getAllocatedRange(), false, false );
+                
+                sheet4.getRange().get("A" + 1).setText("Alapadat");
+                sheet4.getRange().get("F" + 1).setText("Beszerzési cik szállító");
+                sheet4.getRange().get("K" + 1).setText("Cikk katalógus");
+                sheet4.getRange().get("A" + 2).setText("Cikksz.");
+                sheet4.getRange().get("B" + 2).setText("Cikk megnevezése");
+                sheet4.getRange().get("C" + 2).setText("ME szám");
+                sheet4.getRange().get("D" + 2).setText("Gyártó neve");
+                sheet4.getRange().get("E" + 2).setText("Gyártói cikkszám");
+                sheet4.getRange().get("F" + 2).setText("Cikksám");
+                sheet4.getRange().get("G" + 2).setText("Gyártó száma");
+                sheet4.getRange().get("H" + 2).setText("Gyártó cikkszáma");
+                sheet4.getRange().get("I" + 2).setText("Preferált gyártó cikke");
+                sheet4.getRange().get("J" + 2).setText("Jóváhagyva");
+                sheet4.getRange().get("K" + 2).setText("Cikksám");
+                sheet4.getRange().get("L" + 2).setText("Gyártó száma");
+                sheet4.getRange().get("M" + 2).setText("Gyártó cikkszáma");
+                sheet4.getRange().get("N" + 2).setText("Preferált gyártó cikke");
+                sheet4.getRange().get("O" + 2).setText("Jóváhagyva");
+                int cellaszam = 3;
+                for(int szamlalo = 0; szamlalo < datatable.getRows().size(); szamlalo++)
+                {
+                    cellaszam = sheet4.getLastRow()+1;
+                    sheet4.getRange().get("A" + cellaszam).setText(datatable.getRows().get(szamlalo).getString(0));
+                    sheet4.getRange().get("B" + cellaszam).setText(datatable.getRows().get(szamlalo).getString(1));
+                    sheet4.getRange().get("C" + cellaszam).setText(datatable.getRows().get(szamlalo).getString(2));
+                    sheet4.getRange().get("D" + cellaszam).setText(datatable.getRows().get(szamlalo).getString(3));
+                    sheet4.getRange().get("E" + cellaszam).setText(datatable.getRows().get(szamlalo).getString(4));
+                    int utolsosor = cellaszam;
+                    int jovahagyva = 0;
+                    for(int szamlalo2 = 0; szamlalo2 < datatable2.getRows().size(); szamlalo2++)
+                    {
+                        if(datatable.getRows().get(szamlalo).getString(4).equals(datatable2.getRows().get(szamlalo2).getString(2)))
+                        {
+                            if(datatable2.getRows().get(szamlalo2).getString(4).equals("Igen"))
+                            {
+                               jovahagyva++;
+                            }
+                        }
+                    }
+                    if(jovahagyva == 0)
+                    {
+                        sheet4.getRange().get("E" + cellaszam).getStyle().setColor(Color.red);;
+                    }
+                    for(int szamlalo2 = 0; szamlalo2 < datatable2.getRows().size(); szamlalo2++)
+                    {
+                        if(datatable.getRows().get(szamlalo).getString(0).equals(datatable2.getRows().get(szamlalo2).getString(0)))
+                        {
+                            sheet4.getRange().get("F" + cellaszam).setText(datatable2.getRows().get(szamlalo2).getString(0));
+                            sheet4.getRange().get("G" + cellaszam).setText(datatable2.getRows().get(szamlalo2).getString(1));
+                            sheet4.getRange().get("H" + cellaszam).setText(datatable2.getRows().get(szamlalo2).getString(2));
+                            sheet4.getRange().get("I" + cellaszam).setText(datatable2.getRows().get(szamlalo2).getString(3));
+                            sheet4.getRange().get("J" + cellaszam).setText(datatable2.getRows().get(szamlalo2).getString(4));
+                            cellaszam++;
+                        }
+                    }
+                    cellaszam = utolsosor;
+                    
+                    jovahagyva = 0;
+                    for(int szamlalo2 = 0; szamlalo2 < datatable3.getRows().size(); szamlalo2++)
+                    {
+                        if(datatable.getRows().get(szamlalo).getString(4).equals(datatable3.getRows().get(szamlalo2).getString(2)))
+                        {
+                            if(datatable3.getRows().get(szamlalo2).getString(4).equals("Igen"))
+                            {
+                               jovahagyva++;
+                            }
+                        }
+                    }
+                    if(jovahagyva == 0)
+                    {
+                        sheet4.getRange().get("E" + cellaszam).getStyle().setColor(Color.yellow);;
+                    }
+                    for(int szamlalo3 = 0; szamlalo3 < datatable3.getRows().size(); szamlalo3++)
+                    {
+                        if(datatable.getRows().get(szamlalo).getString(0).equals(datatable3.getRows().get(szamlalo3).getString(0)))
+                        {
+                            sheet4.getRange().get("K" + cellaszam).setText(datatable3.getRows().get(szamlalo3).getString(0));
+                            sheet4.getRange().get("L" + cellaszam).setText(datatable3.getRows().get(szamlalo3).getString(1));
+                            sheet4.getRange().get("M" + cellaszam).setText(datatable3.getRows().get(szamlalo3).getString(2));
+                            sheet4.getRange().get("N" + cellaszam).setText(datatable3.getRows().get(szamlalo3).getString(3));
+                            sheet4.getRange().get("O" + cellaszam).setText(datatable3.getRows().get(szamlalo3).getString(4));
+                            cellaszam++;
+                        }
+                    }
+                }
+                
+                sheet4.getAutoFilters().setRange(sheet2.getCellRange("A1:Z1"));
+                sheet4.getAllocatedRange().autoFitColumns();
+                sheet4.getAllocatedRange().autoFitRows();
+                sheet4.getCellRange("A1:Z1").getCellStyle().getExcelFont().isBold(true);                          // félkövér beállítás
+                String hova = System.getProperty("user.home") + "\\Desktop\\Összefésült.xlsx";
+                workbook4.saveToFile(hova, ExcelVersion.Version2016);
+                FileInputStream fileStream = new FileInputStream(hova);
+                try (XSSFWorkbook workbook5 = new XSSFWorkbook(fileStream)) 
+                {
+                    for(int i = workbook5.getNumberOfSheets()-1; i > 0 ;i--)
+                    {    
+                        workbook5.removeSheetAt(i); 
+                    }      
+                    FileOutputStream output = new FileOutputStream(hova);
+                    workbook5.write(output);
+                    output.close();
+                }
+                JOptionPane.showMessageDialog(null, "Kész! \n Mentve az asztalra!", "Info", 1); 
+                Foablak.frame.setCursor(null);  
+            }                        
             catch(Exception e1)
             { 
                 System.out.println(e1);
